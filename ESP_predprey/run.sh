bd2007f4ee12f4fea8ee90f3fe64ac1d9d1272f7
Anand <anandtrex@users.noreply.github.com>
2011-12-11 04:11:18 -0600

    Added cmake support

--- ESP_predprey/run.sh
+++ ESP_predprey/run.sh
@@ -4,5 +4,5 @@
 # export GLOG_stderrthreshold=2
 # export GLOG_logtostderr=1
 export LD_LIBRARY_PATH=`pwd`/glog/lib:`pwd`/libconfig/lib:$LD_LIBRARY_PATH
-./esp-predprey config_file.txt 
+build/esp-predprey config_file.txt 
  -
