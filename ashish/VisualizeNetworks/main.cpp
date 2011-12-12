a0eafd3ff3007c4058de6e23f7613a4807792383
Ashish Jain <ajain@cs.utexas.edu>
2011-12-12 08:27:21 -0600

    visualize networks code is now satisfactory

--- ashish/VisualizeNetworks/main.cpp
+++ ashish/VisualizeNetworks/main.cpp
@@ -12,6 +12,7 @@
 // using
 // std
 using std::ifstream;
+using std::ofstream;
 using std::string;
 using std::cerr;
 using std::endl;
@@ -23,7 +24,7 @@ using NEAT::Gene;
 using NEAT::Organism;
 using NEAT::Network;
 // declarations
-void convertToGraphVizFormat( Genome* ptrGenome );
+void neat2dot( string pathFileDot, Genome* ptrGenome );
 // main
 int main( int argc, char* argv[] ) {
   if ( argc < 2 ) {
@@ -44,27 +45,115 @@ int main( int argc, char* argv[] ) {
   Genome* ptrGenome = new Genome( genomeId, fin );
   fin.close();
   ptrGenome->print_to_file( cout ); // print to console
-  Organism* ptrOrganism = new Organism( 0, ptrGenome, 1 );
-  cout << "Organism" << endl;
-  ptrOrganism->write_to_file( cout );
-  Network* ptrNetwork = ptrOrganism->net;
-  cout << "Network node count: " << ptrNetwork->nodecount() << endl;
-  string pathFileLinks = pathGenome + ".links.txt";
-  ptrNetwork->print_links_tofile( const_cast<char*> ( pathFileLinks.c_str() ) );
+  //Organism* ptrOrganism = new Organism( 0, ptrGenome, 1 );
+  //cout << "Organism" << endl;
+  //ptrOrganism->write_to_file( cout );
+  //Network* ptrNetwork = ptrOrganism->net;
+  //cout << "Network node count: " << ptrNetwork->nodecount() << endl;
+  //string pathFileLinks = pathGenome + ".links.txt";
+  //ptrNetwork->print_links_tofile( const_cast<char*> ( pathFileLinks.c_str() ) );
   string pathFileDot = pathGenome + ".dot";
-  ptrNetwork->writeToDotFile( const_cast<char*> ( pathFileDot.c_str() ) );
-  delete ptrOrganism; //  genome deleted automatically by organism object. No need for delete ptrGenome;
+  //ptrNetwork->writeToDotFile( const_cast<char*> ( pathFileDot.c_str() ) );
+
+  cout << "NEAT2DOT" << endl;
+  neat2dot( pathFileDot, ptrGenome );
+  //delete ptrOrganism; //  genome deleted automatically by organism object. No need for delete ptrGenome;
+  delete ptrGenome;
   return 0;
 }
 
 // other function definitions
-void convertToGraphVizFormat( Genome* ptrGenome ) {
+void neat2dot( string pathFileDot, Genome* ptrGenome ) {
 	// typedefs
-	typedef vector<Gene*> VG;
-	typedef VG::const_iterator VGCI;
+  using NEAT::NNode;
+  using NEAT::Trait;
+  using NEAT::Link;
+  using NEAT::OUTPUT;
+  using NEAT::HIDDEN;
+  using NEAT::INPUT;
+  using NEAT::BIAS;
+  typedef vector<NNode*> VPN;
+  typedef VPN::iterator VPNI;
+  typedef VPN::const_iterator VPNCI;
+  typedef vector<Trait*> VPT; 
+  typedef VPT::iterator VPTI;
+  typedef vector<Gene*> VPG;
+  typedef VPG::iterator VPGI;
+  ofstream fout( pathFileDot.c_str() );
+  if ( !fout.is_open() ) {
+    cerr << "Unable to open file " << pathFileDot << " for writing!" << endl;
+    throw 1; // throw something meaningful later
+  }
 	// simply iterate through all nodes of the genome for now
-	VG &vg = ptrGenome->genes;
-	for ( VGCI it = vg.begin(); it != vg.end(); ++it )
-		;
+  string indentation = "\t";
+  string terminate = ";\n";
+	fout << "digraph G {\n";
+  fout << indentation << "rankdir=BT" << terminate;
+  fout << indentation << "node [shape=circle, weight=bold, fontsize=10, color=black, fontcolor=black]" << terminate;
+  fout << indentation << "edge [color=black, fontsize=9, fontcolor=black, decorate = true ]" << terminate;
+  VPN& vNodes = ptrGenome->nodes;
+  // output nodes
+  fout << indentation << "node [ shape = ellipse ]" << terminate;
+  for ( VPNI itN = vNodes.begin(); itN != vNodes.end(); ++itN ) {
+    if ( OUTPUT == (*itN)->gen_node_label ) {
+      int nodeId = (*itN)->node_id;
+      fout << indentation << nodeId << " [ label=\"O" << nodeId << "\" ]" << terminate; 
+    }
+  }
+  // input nodes
+  fout << indentation << "node [ shape = box ]" << terminate;
+  for ( VPNI itN = vNodes.begin(); itN != vNodes.end(); ++itN ) {
+    if ( INPUT == (*itN)->gen_node_label ) {
+      int nodeId = (*itN)->node_id;
+      fout << indentation << nodeId << " [ label=\"I" << nodeId << "\" ]" << terminate; 
+    }
+  }
+  // bias nodes
+  fout << indentation << "node [ shape = hexagon ]" << terminate;
+  for ( VPNI itN = vNodes.begin(); itN != vNodes.end(); ++itN ) {
+    if ( BIAS == (*itN)->gen_node_label ) {
+      int nodeId = (*itN)->node_id;
+      fout << indentation << nodeId << " [ label=\"B" << nodeId << "\" ]" << terminate; 
+    }
+  }
+  // hidden nodes
+  fout << indentation << "node [ shape = circle ]" << terminate;
+  for ( VPNI itN = vNodes.begin(); itN != vNodes.end(); ++itN ) {
+    if ( HIDDEN == (*itN)->gen_node_label ) {
+      int nodeId = (*itN)->node_id;
+      fout << indentation << nodeId << " [ label=\"H" << nodeId << "\" ]" << terminate; 
+    }
+  }
+  // only for output nodes
+  fout << indentation << "{ rank=sink; "; 
+  for ( VPNI itN = vNodes.begin(); itN != vNodes.end(); ++itN ) {
+    if ( OUTPUT == (*itN)->gen_node_label ) {
+      int nodeId = (*itN)->node_id;
+      fout << nodeId << "; "; 
+    }
+  }
+  fout << "}" << endl;
+  // only for input nodes
+  fout << indentation << "{ rank=source; "; 
+  for ( VPNI itN = vNodes.begin(); itN != vNodes.end(); ++itN ) {
+    if ( INPUT == (*itN)->gen_node_label || BIAS == (*itN)->gen_node_label ) {
+      int nodeId = (*itN)->node_id;
+      fout << nodeId << "; "; 
+    }
+  }
+  fout << "}" << endl;
+  // links
+  VPG& vGenes = ptrGenome->genes;
+  for ( VPGI itG = vGenes.begin(); itG != vGenes.end(); ++itG ) {
+    Gene& gene = **itG;
+    Link& link = *( gene.lnk );
+    int nodeIdIn, nodeIdOut;
+    nodeIdIn = link.in_node->node_id;
+    nodeIdOut = link.out_node->node_id;
+    fout << indentation << nodeIdIn << " -> " << nodeIdOut; 
+    fout << " [ label = " << link.weight << " ]" << terminate;
+  }
+  fout << "}\n";
+  fout.close();
 }
 
