5ee6a16f2f3cc7ac92eb6feac529f50c4bfb635a
Ashish Jain <ajain@cs.utexas.edu>
2011-12-13 05:32:50 -0600

    further tested with random predator
    Guess what -- well maybe I am tired so I wasn't expecting it but the hunter
    caught the predator. So everything is working. This random predator was a
    double check.

--- ashish/PreyHunter/Basic.cpp
+++ ashish/PreyHunter/Basic.cpp
@@ -3,6 +3,7 @@
 
 namespace PredatorPreyHunter {
   // wrapper for drand48() so that you can change this function later
+  // fetch random number between 0 and 1
   double fetchRandomNumber() {
     return drand48();
   }
